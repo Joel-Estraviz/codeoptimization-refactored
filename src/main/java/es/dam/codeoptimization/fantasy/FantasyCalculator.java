4bc2a6d25344cad2253845d90d86e137f8323dd9
Boris <boris.caballero@rai.usc.es>
2026-04-21 15:06:56 +0200

    Extracted common calculations out of the position cases

--- src/main/java/es/dam/codeoptimization/fantasy/FantasyCalculator.java
+++ src/main/java/es/dam/codeoptimization/fantasy/FantasyCalculator.java
@@ -3,16 +3,18 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package es.dam.codeoptimization.fantasy;
+
 import es.dam.codeoptimization.PlayerStats;
 
 /**
  * THE CLASS YOU HAVE TO MODIFY
+ *
  * @author Boris
  */
 public class FantasyCalculator {
 
     public static int calculatePoints(PlayerStats stats) {
-        int result = 0;         
+        int result = 0;
         int minutes = stats.minutes;
         int goals = stats.goals;
         int assists = stats.assists;
@@ -22,43 +24,31 @@ public class FantasyCalculator {
         int goalsAgainst = stats.goalsAgainst;
         char matchResult = stats.matchResult;
         String position = stats.position;
-        
+
         final String GOALKEEPER_STRING = "PORTERO";
         final String DEFENCE_STRING = "DEFENSA";
         final String MIDFIELDER_STRING = "MEDIO";
         final String FORWARD_STRING = "DELANTERO";
 
+        result = calculateMinutesPlayed(minutes, result);
+        result = calculateYellowCard(yellowCard, result);
+        result = calculateRedCard(redCard, result);
+        result = calculateMatchResult(matchResult, result);
         if (position.equals(GOALKEEPER_STRING)) {
-            result = calculateMinutesPlayed(minutes, result);
             result = calculateGoals(goals, result);
             result = calculateAssists(result, assists);
-            result = calculateSaves(result, saves);            
+            result = calculateSaves(result, saves);
             result = calculateGoalsReceived(goalsAgainst, result);
-            result = calculateYellowCard(yellowCard, result); 
-            result = calculateRedCard(redCard, result);            
-            result = calculateMatchResult(matchResult, result);
         } else if (position.equals(DEFENCE_STRING)) {
-            result = calculateMinutesPlayed(minutes, result);
-            result = calculateGoals(goals, result);
-            result = calculateAssists(result, assists);
             result = calculateGoalsReceived(goalsAgainst, result);
-            result = calculateYellowCard(yellowCard, result);
-            result = calculateRedCard(redCard, result);            
-            result = calculateMatchResult(matchResult, result);
-        } else if (position.equals(MIDFIELDER_STRING)) {
-            result = calculateMinutesPlayed(minutes, result);
             result = calculateGoals(goals, result);
             result = calculateAssists(result, assists);
-            result = calculateYellowCard(yellowCard, result);
-            result = calculateRedCard(redCard, result);            
-            result = calculateMatchResult(matchResult, result);
+        } else if (position.equals(MIDFIELDER_STRING)) {
+            result = calculateGoals(goals, result);
+            result = calculateAssists(result, assists);
         } else if (position.equals(FORWARD_STRING)) {
-            result = calculateMinutesPlayed(minutes, result);
             result = calculateGoalsForward(goals, result);
             result = calculateAssistsForward(result, assists);
-            result = calculateYellowCard(yellowCard, result);
-            result = calculateRedCard(redCard, result);            
-            result = calculateMatchResult(matchResult, result);
         }
 
         return result;
@@ -66,28 +56,28 @@ public class FantasyCalculator {
 
     private static int calculateAssistsForward(int result, int assists) {
         final int POINTS_PER_ASSIST = 5;
-        
+
         result = result + (assists * POINTS_PER_ASSIST);
         return result;
     }
 
     private static int calculateAssists(int result, int assists) {
         final int POINTS_PER_ASSIST = 6;
-        
+
         result = result + (assists * POINTS_PER_ASSIST);
         return result;
     }
 
     private static int calculateSaves(int result, int saves) {
         final int POINTS_PER_SAVE = 1;
-        
+
         result = result + saves * POINTS_PER_SAVE;
         return result;
     }
 
     private static int calculateGoalsForward(int goals, int result) {
         final int POINTS_PER_GOAL = 6;
-        
+
         for (int i = 0; i < goals; i++) {
             result = result + POINTS_PER_GOAL;
         }
@@ -99,7 +89,7 @@ public class FantasyCalculator {
         final char MATCH_DRAW = 'E';
         final int POINTS_FOR_WINNING_MATCH = 5;
         final int POINTS_FOR_DRAWING_MATCH = 2;
-        
+
         if (matchResult == MATCH_WON) {
             result = result + POINTS_FOR_WINNING_MATCH;
         } else if (matchResult == MATCH_DRAW) {
@@ -110,15 +100,19 @@ public class FantasyCalculator {
 
     private static int calculateRedCard(boolean redCard, int result) {
         final int POINTS_FOR_RED_CARD = -5;
-        
-        if (redCard == true) result = result + POINTS_FOR_RED_CARD;
+
+        if (redCard == true) {
+            result = result + POINTS_FOR_RED_CARD;
+        }
         return result;
     }
 
     private static int calculateYellowCard(boolean yellowCard, int result) {
         final int POINTS_FOR_YELLOW_CARD = -3;
-        
-        if (yellowCard == true) result = result + POINTS_FOR_YELLOW_CARD;
+
+        if (yellowCard == true) {
+            result = result + POINTS_FOR_YELLOW_CARD;
+        }
         return result;
     }
 
@@ -126,7 +120,7 @@ public class FantasyCalculator {
         final int POINTS_FOR_ZERO_GOALS_RECEIVED = 5;
         final int POINTS_FOR_ONE_GOAL_RECEIVED = 3;
         final int POINTS_FOR_TWO_GOALS_RECEIVED = 1;
-        
+
         if (goalsAgainst == 0) {
             result = result + POINTS_FOR_ZERO_GOALS_RECEIVED;
         } else if (goalsAgainst == 1) {
@@ -139,7 +133,7 @@ public class FantasyCalculator {
 
     private static int calculateGoals(int goals, int result) {
         final int POINTS_PER_GOAL = 5;
-        
+
         for (int i = 0; i < goals; i++) {
             result = result + POINTS_PER_GOAL;
         }
@@ -150,7 +144,7 @@ public class FantasyCalculator {
         final int POINTS_PLAYED_SOME_MINUTES = 3;
         final int POINTS_PLAYED_MOST_MINUTES = 5;
         final int MINUTES_LIMIT_FOR_MOST_MINUTES = 60;
-        
+
         if (minutes > 0 && minutes < MINUTES_LIMIT_FOR_MOST_MINUTES) {
             result = result + POINTS_PLAYED_SOME_MINUTES;
         } else if (minutes >= MINUTES_LIMIT_FOR_MOST_MINUTES) {
